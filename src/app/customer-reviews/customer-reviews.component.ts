9df02fdc1c70558e861db396935915a844ab8be0
PUSHPDEV PATEL <pushpdevpatel.in@gmail.com>
2024-07-13 08:23:57 +0530

    first commit

--- src/app/customer-reviews/customer-reviews.component.ts
+++ src/app/customer-reviews/customer-reviews.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class CustomerReviewsComponent {
 
+  customerReview: any[] = [
+    {
+      imgUrl: '../../assets/customer-reviews/customer-1.jpg',
+      description: ' As I sign off, permit me to record my deepest appreciation for Siya from your call centre team for working hard these past few months to evolve our earlier discount-based on-off relationship into today’s all-weather version. In my considered assessment, she is an asset to the DevMeds customer-facing team, and secures from me a heads-up CSAT rating of 10/10.',
+      customerName: 'Pushpdev Patel',
+      customerPosition: 'Patient'
+    },
+    {
+      imgUrl: '../../assets/customer-reviews/customer-1.jpg',
+      description: ' As I sign off, permit me to record my deepest appreciation for Siya from your call centre team for working hard these past few months to evolve our earlier discount-based on-off relationship into today’s all-weather version. In my considered assessment, she is an asset to the DevMeds customer-facing team, and secures from me a heads-up CSAT rating of 10/10.',
+      customerName: 'Pushpdev Patel',
+      customerPosition: 'Patient'
+    },
+    {
+      imgUrl: '../../assets/customer-reviews/customer-1.jpg',
+      description: ' As I sign off, permit me to record my deepest appreciation for Siya from your call centre team for working hard these past few months to evolve our earlier discount-based on-off relationship into today’s all-weather version. In my considered assessment, she is an asset to the DevMeds customer-facing team, and secures from me a heads-up CSAT rating of 10/10.',
+      customerName: 'Pushpdev Patel',
+      customerPosition: 'Patient'
+    },
+    {
+      imgUrl: '../../assets/customer-reviews/customer-1.jpg',
+      description: ' As I sign off, permit me to record my deepest appreciation for Siya from your call centre team for working hard these past few months to evolve our earlier discount-based on-off relationship into today’s all-weather version. In my considered assessment, she is an asset to the DevMeds customer-facing team, and secures from me a heads-up CSAT rating of 10/10.',
+      customerName: 'Pushpdev Patel',
+      customerPosition: 'Patient'
+    },
+    {
+      imgUrl: '../../assets/customer-reviews/customer-1.jpg',
+      description: ' As I sign off, permit me to record my deepest appreciation for Siya from your call centre team for working hard these past few months to evolve our earlier discount-based on-off relationship into today’s all-weather version. In my considered assessment, she is an asset to the DevMeds customer-facing team, and secures from me a heads-up CSAT rating of 10/10.',
+      customerName: 'Pushpdev Patel',
+      customerPosition: 'Patient'
+    },
+  ]
 }
